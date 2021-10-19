db8aa55ffb85fa81a01804a0e1fef3da4579a975
SokurenkoED <SokurenkoED@yandex.ru>
2021-10-19 20:13:46 +0300

    Сделал тест №1

--- SVBU_Test/Program.cs
+++ SVBU_Test/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SVBU_Test.Tests;
+using System;
+using System.IO;
 
 namespace SVBU_Test
 {
@@ -6,7 +8,28 @@ namespace SVBU_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string PathToXML = "!List_SVBU.xml"; // Путь до файла с данными
+
+            #region Создаем дирректорию
+
+            DirectoryInfo dirInfo = new DirectoryInfo("Report");
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            #endregion
+
+            try
+            {
+                IS_IN_OUT Test1 = new IS_IN_OUT(PathToXML);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Файл {0} не был найден", PathToXML);
+            }
+
+            Console.ReadKey(true);
         }
     }
 }
---
+++ SVBU_Test/Tests/IS_IN_OUT.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace SVBU_Test.Tests
+{
+    class Elem
+    {
+        public Elem(string NameIs, string NameALGH)
+        {
+            this.NameIs = NameIs;
+            this.NameALGH = NameALGH;
+        }
+        public string NameIs { get; set; }
+        public string NameALGH { get; set; }
+    }
+
+    class IS_IN_OUT
+    {
+        public IS_IN_OUT(string Path)
+        {
+            XDocument xdoc = XDocument.Load(Path);
+            List<Elem> IS_OUT = new List<Elem>();
+            List<string> IS_IN = new List<string>();
+            string IsSuccess = "УСПЕШНО";
+
+            #region Находим все элементы IS_OUT и IS_INP
+
+            foreach (XElement ALGHs in xdoc.Element("LAES-2").Element("VERSION").Elements("ALGORITHM"))
+            {
+                foreach (var item in ALGHs.Descendants("IS_OUT"))
+                {
+                    IS_OUT.Add(new Elem(item.Attribute("name").Value, ALGHs.Attribute("name").Value));
+                }
+                foreach (var item in ALGHs.Descendants("IS_INP"))
+                {
+                    IS_IN.Add(item.Attribute("name").Value.Split(new char[] { '#' }, StringSplitOptions.RemoveEmptyEntries)[0]);
+                }
+            }
+
+            #endregion
+
+            Console.WriteLine("Тест №1: Поиск входных сигналов по выходным - начался!"); Console.WriteLine();
+
+            #region Алгоритм проверки
+
+            foreach (var Out in IS_OUT)
+            {
+                if (!IS_IN.Contains(Out.NameIs.Split(new char[] { '#' }, StringSplitOptions.RemoveEmptyEntries)[0]))
+                {
+                    Console.WriteLine("Ошибка. Отсутствует входной внутренний сигнал: {0}. Выходной внутренний сигнал присутствует в алгоритме: {1}", Out.NameIs, Out.NameALGH);
+                    IsSuccess = "С ОШИБКАМИ";
+                }
+            }
+
+            #endregion
+
+            Console.WriteLine(); Console.WriteLine("Тест №1: Поиск входных сигналов по выходным - закончился {0}!", IsSuccess);
+        }
+    }
+}
